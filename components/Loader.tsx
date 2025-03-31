04e99720b4e36cf5cfbf5d8da891e5764067e7b5
unknown <uaepeekawariour@gmail.com>
2025-03-31 14:13:39 +0530

    re com
    Text comf cation

--- components/Loader.tsx
+++ components/Loader.tsx
@@ -4,7 +4,7 @@ import Name from '../public/Svg/Loader/Name'
 const Loader = () => {
   return (
     <div className='flex w-full h-screen justify-center items-center'>
-      <Name text='Alfarhaankhan' />
+      <Name text="Alfarhaankhan" />
     </div>
   )
 }
--- public/Svg/Loader/Name.tsx
+++ public/Svg/Loader/Name.tsx
@@ -1,29 +1,15 @@
 "use client"
 import { motion } from "motion/react"
-import { useState } from "react";
-const Name = ({ text }: { text: string }) => {
-  const letters = text.split("");
-
+const Name = ({ text, delay = 0.2, duration = 1.5 }) => {
   return (
-    <div className="flex justify-center items-center overflow-hidden">
-      <motion.div className="flex">
-        {letters.map((letter, index) => (
-          <motion.span
-            key={index}
-            className="textCol text-8xl font-DSEN leading-none"
-            initial={{ opacity: 0, y: 20, scaleY: 0 }}
-            animate={{ opacity: 1, y: 0, scaleY: 1 }}
-            transition={{
-              duration: 0.5,
-              ease: "easeInOut",
-              delay: index * 0.1,
-            }}
-          >
-            {letter}
-          </motion.span>
-        ))}
-      </motion.div>
-    </div>
+    <motion.div
+      initial={{ opacity: 0, y: 10 }}
+      animate={{ opacity: 1, y: 0 }}
+      transition={{ duration, delay, ease: "easeOut" }}
+      className="text-8xl font-DSEN textCols"
+    >
+      {text}
+    </motion.div>
   );
 };
 
